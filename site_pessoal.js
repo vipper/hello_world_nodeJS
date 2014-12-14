6bd636ffdcc773b49989258c36eaab8aa63b3c07
Daniel Ricardo de Amorim <amorim.danielricardo@gmail.com>
2014-12-14 19:35:37 -0200

    Terminando a primeira parte do hello_world

---
+++ site_pessoal.js
@@ -0,0 +1,16 @@
+var http = require("http");
+var fs = require("fs");
+
+var server = http.createServer(function(request, response) {
+
+	fs.readFile(__dirname + "/index.html", function(error, html) {
+		response.writeHead(200, {"Content-Type" : "text/html"});
+		response.write(html);
+		response.end();
+	});
+
+});
+
+server.listen(3000, function() {
+	console.log("Executando Site Pessoal");
+})
