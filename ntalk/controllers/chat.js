1fae836b37881e5510f39f27748f0e99f0d84141
Daniel Ricardo de Amorim <amorim.danielricardo@gmail.com>
2015-03-19 10:24:20 -0300

    Adicionando sala para conversa no chat

--- ntalk/controllers/chat.js
+++ ntalk/controllers/chat.js
@@ -1,7 +1,7 @@
 module.exports = function(app) {
 	var ChatController = {
 		index: function(req, res) {
-			var params = {email: req.params.email};
+			var params = {sala: req.query.sala};
 			res.render('chat/index', params);
 		}
 	};
--- ntalk/sockets/chat.js
+++ ntalk/sockets/chat.js
@@ -1,12 +1,37 @@
 module.exports = function(io) {
-	var sockets = io.sockets;
-	sockets.on('connection', function(client) {
-		var session = client.handshake.session,
-			usuario = session.usuario;
-		client.on('send-server', function(msg) {
-			var msg = "<b>" + usuario.nome + "</b> " + msg + "<br>";
-			client.emit('send-client', msg);
-			client.broadcast.emit('send-client', msg);
-		});
-	});
+  var crypto = require('crypto')
+    , sockets = io.sockets
+  ;
+  sockets.on('connection', function (client) {
+    var session = client.handshake.session
+      , usuario = session.usuario
+    ;
+
+    client.on('join', function(sala) {
+      if(!sala) {
+        var timestamp = new Date().toString()
+          , md5 = crypto.createHash('md5')
+        ;
+        sala = md5.update(timestamp).digest('hex');
+      }
+      session.sala = sala;
+      client.join(sala);
+    });
+
+    client.on('disconnect', function () {
+      var sala = session.sala
+        , msg = "<b>"+ usuario.nome +":</b> saiu.<br>";
+      client.broadcast.emit('notify-offlines', usuario.email);
+      sockets.in(sala).emit('send-client', msg);
+      client.leave(sala);
+    }); 
+
+    client.on('send-server', function (msg) {
+      var sala = session.sala
+        , data = {email: usuario.email, sala: sala};
+      msg = "<b>"+usuario.nome+":</b> "+msg+"<br>";
+      client.broadcast.emit('new-message', data);
+      sockets.in(sala).emit('send-client', msg);
+    });
+  });
 }
